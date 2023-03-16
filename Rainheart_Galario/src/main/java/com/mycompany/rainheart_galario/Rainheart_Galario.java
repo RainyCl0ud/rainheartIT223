9eb99929fddb52a2730f4eff913f1cbabe3cd768
RainyCl0ud <128013461+RainyCl0ud@users.noreply.github.com>
2023-03-16 11:44:50 +0800

    Add files via upload

---
+++ Rainheart_Galario/src/main/java/com/mycompany/rainheart_galario/Rainheart_Galario.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.rainheart_galario;
+
+/**
+ *
+ * @author Jayde
+ */
+public class Rainheart_Galario {
+
+         public static void main(String[] args) {
+        
+         int a = 5;
+         int b = 5;
+         
+         int d = a+b;
+      
+  
+      System.out.println( "The answer is : " + d);
+        
+    }
+    
+}
